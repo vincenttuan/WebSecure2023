ba5f69862b9f1240fc5d419d1865f2f41d349630
vincenttuan <vincentjava@yahoo.com.tw>
2023-02-12 19:16:11 +0800

    secure

---
+++ Web-9443/src/main/java/com/example/demo/Web9443Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web9443Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web9443Application.class, args);
+	}
+
+}
