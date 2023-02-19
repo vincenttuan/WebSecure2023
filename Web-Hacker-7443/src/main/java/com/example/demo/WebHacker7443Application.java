1bb8a8ef7eef4cbe863c9bf0cf5a6f47a63e5c8d
vincenttuan <vincentjava@yahoo.com.tw>
2023-02-19 21:53:42 +0800

    hacker

---
+++ Web-Hacker-7443/src/main/java/com/example/demo/WebHacker7443Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHacker7443Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebHacker7443Application.class, args);
+	}
+
+}
