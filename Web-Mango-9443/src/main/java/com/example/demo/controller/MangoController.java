ed4791c945590a02016c7e2060ffabefcb387de3
vincenttuan <vincentjava@yahoo.com.tw>
2023-02-19 20:34:48 +0800

    mango

--- Web-Mango-9443/src/main/java/com/example/demo/controller/MangoController.java
+++ Web-Mango-9443/src/main/java/com/example/demo/controller/MangoController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/mango")
 public class MangoController {
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@RequestMapping("/input")
 	public String input(Model model, HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("input");
+		String myName = request.getParameter("myName");
+		System.out.println("myName: " + myName);
+		
+		List list  = null;
+		String sql = "select myname, amount, cardNo, memo from Mango where myname='" + myName + "'";
+		System.out.println("sql: " + sql);
+		list = jdbcTemplate.queryForList(sql);
+		
+		model.addAttribute("myName", myName);
+		model.addAttribute("list", list);
 		return "mango/input";
 	}
 	
